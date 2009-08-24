dce614ddcb0594ea6105740ec44984412c5d2629
Jessica Tsuji <jessicatsuji@jessica-tsujis-macbook-pro.local>
2009-08-24 14:34:29 -0700

    set up Controllers, finished About page style and structure, finished footer styles, pulled in tweet & fixed feat work arrow interaction

--- app/controllers/AboutController.php
+++ app/controllers/AboutController.php
@@ -1,12 +1,12 @@
 <?php
-	class IndexController extends Zend_Controller_Action
+	class AboutController extends Zend_Controller_Action
 	{
 		public function init() {
 			//$this->session_alert = new Zend_Session_Namespace('');
 			//$this->Model = new Model();
 			//$this->_helper->layout->setLayout('edit');
 			
-			$this->view->page = "home";
+			$this->view->page = "about";
 		}
 		
 		public function indexAction()
--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -1,12 +1,12 @@
 <?php
-	class IndexController extends Zend_Controller_Action
+	class ContactController extends Zend_Controller_Action
 	{
 		public function init() {
 			//$this->session_alert = new Zend_Session_Namespace('');
 			//$this->Model = new Model();
 			//$this->_helper->layout->setLayout('edit');
 			
-			$this->view->page = "home";
+			$this->view->page = "contact";
 		}
 		
 		public function indexAction()
